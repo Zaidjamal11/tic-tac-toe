c0daf44c6139ab22b96c8bcb626b0b2bf2dbf02f
unknown <zaidj777@gmail.com>
2023-07-16 17:18:53 +0530

    Game Logic Implemented

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,15 +1,27 @@
-import { Button } from "semantic-ui-react";
-import { deflateSync } from "zlib"
 
 
-const Square: React.FC = () => {
-    return (
-        <Button className="board-square">
-            x
-        </Button>
-    )
+interface ICreateNoteProps {
+  onClick: () => void,
+  value: string | null,
+  winner: string | null
 }
 
+const Square: React.FC<ICreateNoteProps> = ({ value, winner, onClick }) => {
 
+  if (!value) {
+      return <button 
+          onClick={onClick} 
+          disabled={Boolean(winner)} 
+          className='board-square'
+      />
+  }
+
+  return (
+      <button 
+          disabled
+          className={`board-square square_${value}`}
+      >{value}</button>
+  )
+}
 
 export default Square;
